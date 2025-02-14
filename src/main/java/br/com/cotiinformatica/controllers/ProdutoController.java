23529823cba0a3b04d721472e8ae2f55e4acd927
renansantanax <renan.2r@live.com>
2025-02-13 21:49:23 -0300

    Consulta de categorias e configurações do swagger e cors

--- src/main/java/br/com/cotiinformatica/controllers/ProdutoController.java
+++ src/main/java/br/com/cotiinformatica/controllers/ProdutoController.java
@@ -7,25 +7,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 	
+	@Operation(summary = "Serviço para cadastrar produto.")
 	@PostMapping("cadastrar")
 	public void cadastrar() {
 		// TODO
 	}
 
+	@Operation(summary = "Serviço para atualizar produto.")
 	@PutMapping("atualizar")
 	public void atualizar() {
 		// TODO 
 	}
 	
+	@Operation(summary = "Serviço para excluir produto.")
 	@DeleteMapping("excluir")
 	public void excluir() {
 		// TODO
 	}
 	
+	@Operation(summary = "Serviço para consultar todos os produtos.")
 	@GetMapping("consultar")
 	public void consultar() {
 		// TODO
--- src/main/java/br/com/cotiinformatica/entities/Categoria.java
+++ src/main/java/br/com/cotiinformatica/entities/Categoria.java
@@ -19,5 +19,6 @@ public class Categoria {
 	private UUID id;
 	private String nome;
 	private List<Produto> produtos;
+	
 
 }
